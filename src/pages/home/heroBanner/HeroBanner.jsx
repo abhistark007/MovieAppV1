1ec22c9b9ee549a2c184e609f3419bbd7f506e84
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-24 20:26:47 +0530

    Created HeroBanner only bgImage is remaining

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function HeroBanner() {
   return (
-    <div>HeroBanner</div>
+    <div className='flex justify-center'>
+        <div className='flex flex-col gap-5 items-center'> 
+          <p className='text-9xl text-white'>Welcome</p>
+          <p className='text-3xl text-white'>Millions of TVSows and movies waiting to be discovered. Explore now!</p>
+          <div className='flex border-2 border-purple-600 rounded-full w-[60%]'>
+            <input type='text' placeholder='Search' className=' rounded-l-full outline-none py-3 px-2 flex-1'/>
+            <button className='bg-purple-600 rounded-r-full text-white px-4 hover:scale-110 duration-200'>Search</button>
+          </div>
+        </div>
+    </div>
   )
 }
 
