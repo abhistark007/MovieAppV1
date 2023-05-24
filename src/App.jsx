59adc946f875b85a15733c096a0855950cdd1117
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-24 20:02:52 +0530

    Setup Routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import { useEffect,useState } from "react";
 import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfiguration,getGenres } from "./store/homeSlice";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
+import PageNotFound from './pages/404/PageNotFound';
+import Details from './pages/details/Details';
+import Explore from './pages/explore/Explore';
+import SearchResult from './pages/searchResult/SearchResult'
 
 function App() {
 
@@ -11,7 +20,7 @@ function App() {
 
 
   useEffect(()=>{
-    apiTesting();
+    
   },[])
 
   const apiTesting=()=>{
@@ -24,9 +33,15 @@ function App() {
 
   return (
     <>
-      <div>
-        {url?.total_pages}
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/:mediaType/:id" element={<Details/>}/>
+          <Route path="/search/:query" element={<SearchResult/>}/>
+          <Route path="/explore/:mediaType" element={<Explore/>}/>
+          <Route path="*" element={<PageNotFound/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
