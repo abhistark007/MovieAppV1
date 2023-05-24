5f76729c054ed0758affb3dc83c8b739974f140f
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-24 14:21:05 +0530

    Update App.jsx
    Testing the api using useEffect and calling the fetDataFromApi function  inside utils folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-
+import { useEffect,useState } from "react";
+import { fetchDataFromApi } from "./utils/api";
 
 function App() {
 
+  useEffect(()=>{
+    apiTesting();
+  },[])
+
+  const apiTesting=()=>{
+    fetchDataFromApi("/movie/popular")
+    .then((response)=>{
+      console.log(response);
+    })
+  }
+
   return (
     <>
-      
+      <div>
+        APP
+      </div>
     </>
   )
 }
