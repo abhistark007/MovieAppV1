272d43eb9bf9210582acc1666192997fc3334bca
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-24 20:55:24 +0530

    Update HeroBanner.jsx
    Added navigate() feature of react-router-dom

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function HeroBanner() {
 
   const [background,setBackground]=useState("");
   const [query,setQuery]=useState("");
 
+  const navigate=useNavigate();
+
   const searchQueryHandler=(e)=>{
     if(e.key==="Enter" && query.length>0){
-
+      navigate(`/search/${query}`);
+    }
+  }
+  const handleClick=()=>{
+    if(query.length>0){
+      navigate(`/search/${query}`);
     }
   }
 
@@ -19,7 +27,7 @@ function HeroBanner() {
           <p className='text-3xl text-white'>Millions of TVSows and movies waiting to be discovered. Explore now!</p>
           <div className='flex border-2 border-purple-600 rounded-full w-[60%]'>
             <input onChange={(e)=>setQuery(e.target.value)} onKeyUp={searchQueryHandler} type='text' placeholder='Search' className=' rounded-l-full outline-none py-3 px-2 flex-1'/>
-            <button className='bg-purple-600 rounded-r-full text-white px-4 hover:scale-110 duration-200'>Search</button>
+            <button onClick={handleClick} className='bg-purple-600 rounded-r-full text-white px-4 hover:scale-110 duration-200'>Search</button>
           </div>
         </div>
     </div>
