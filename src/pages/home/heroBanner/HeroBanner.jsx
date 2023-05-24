1d47eb89d3b309226aae248daa40740bc4f71e19
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-24 20:35:16 +0530

    Update HeroBanner.jsx
    Added background and query state variables and created searchQueryHandler which works only when enter ket is pressed and query is not empty string

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function HeroBanner() {
+
+  const [background,setBackground]=useState("");
+  const [query,setQuery]=useState("");
+
+  const searchQueryHandler=(e)=>{
+    if(e.key==="Enter" && query.length>0){
+
+    }
+  }
+
+
   return (
     <div className='flex justify-center'>
         <div className='flex flex-col gap-5 items-center'> 
           <p className='text-9xl text-white'>Welcome</p>
           <p className='text-3xl text-white'>Millions of TVSows and movies waiting to be discovered. Explore now!</p>
           <div className='flex border-2 border-purple-600 rounded-full w-[60%]'>
-            <input type='text' placeholder='Search' className=' rounded-l-full outline-none py-3 px-2 flex-1'/>
+            <input onChange={(e)=>setQuery(e.target.value)} onKeyUp={searchQueryHandler} type='text' placeholder='Search' className=' rounded-l-full outline-none py-3 px-2 flex-1'/>
             <button className='bg-purple-600 rounded-r-full text-white px-4 hover:scale-110 duration-200'>Search</button>
           </div>
         </div>
