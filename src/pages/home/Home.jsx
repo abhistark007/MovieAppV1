3ec585fe45d8d37f675de05cc431a551e0025c3e
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-24 21:15:24 +0530

    Added a min screen height

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ import HeroBanner from './heroBanner/HeroBanner'
 
 function Home() {
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col min-h-screen'>
       <HeroBanner/>
     </div>
   )
